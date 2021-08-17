81c729a125711f71c59696a968a7840d964f7614
kissato70 <>
2021-08-17 16:20:20 +0200

    Initial commit

--- .bin/init.js
+++ .bin/init.js
@@ -12,6 +12,7 @@ console.log("| React-Library_boilerplate INIT 1.2 |");
 console.log("--------------------------------------");
 
 const packageJSON1 = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const packageJSON2 = JSON.parse(fs.readFileSync('./example/package.json', 'utf8'));
 const html = fs.readFileSync('./example/public/index.html', 'utf8');
 
 // Only run the clean-up if the project is in the initial state
@@ -43,33 +44,51 @@ if (!htmlTitle.match(titleRegexp))
   console.log("\x1b[31m", "ERROR:", "\x1b[0m", "Invalid title name.");
   process.exit(1);
 }
-
+// Change the project name in the root package.json
 packageJSON1.name = projectName;
-packageJSON1.scripts = Object.fromEntries(
-  Object.entries(packageJSON1.scripts).filter(([key, value]) => key !== ('serve')   ));
-packageJSON1.scripts = { ...packageJSON1.scripts, "serve": `PORT=${portNumber} react-scripts start` };
-// update project name in package.json
+// Change the port number in the example package.json
+packageJSON2.scripts = Object.fromEntries(
+  Object.entries(packageJSON2.scripts).filter(([key, value]) => key !== ('serve')   ));
+packageJSON2.scripts = { ...packageJSON2.scripts, "serve": `PORT=${portNumber} node ../node_modules/react-scripts/bin/react-scripts.js start` };
+// Change the linked package's name in the example package.json
+packageJSON2.dependencies = Object.fromEntries(
+  Object.entries(packageJSON2.dependencies).filter(([key, value]) => key !== ('library')   ));
+packageJSON2.dependencies[projectName] = "link:.." ;
+
+// write out the root package.json
 fs.writeFileSync('./package.json', JSON.stringify(packageJSON1, null, "\t"), err =>
 {
   if (err)
   {
-    console.log("\x1b[31m", "ERROR:", "\x1b[0m", "The package.json file can't be written.");
+    console.log("\x1b[31m", "ERROR:", "\x1b[0m", "The root package.json file can't be written.");
+    process.exit(1);
+  }
+})
+// write out the example package.json
+fs.writeFileSync('./example/package.json', JSON.stringify(packageJSON2, null, "\t"), err =>
+{
+  if (err)
+  {
+    console.log("\x1b[31m", "ERROR:", "\x1b[0m", "The root package.json file can't be written.");
     process.exit(1);
   }
 })
 console.log(" Project name and the server port has been changed in package.json file.");
 
-const newHtml = html.replace("<title>Kekalma React ROLLUP Boilerplate</title>", `<title>${htmlTitle}</title>`);
+const newHtml = html.replace("<title>LIBRARY Example</title>", `<title>${htmlTitle}</title>`);
 // update the title in index.html
-fs.writeFileSync('./public/index.html', newHtml , err =>
+fs.writeFileSync('./example/public/index.html', newHtml , err =>
 {
   if (err)
   {
-    console.log("\x1b[31m", "ERROR:", "\x1b[0m", "The index.html file can't be written.");
+    console.log("\x1b[31m", "ERROR:", "\x1b[0m", "The example index.html file can't be written.");
     process.exit(1);
   }
 })
 console.log(" Project title has been changed in index.html file.");
+
+console.log("VÉGE...........")
+process.exit(0)
 
 // Delete the Git folder
 fs.rm(".git", { recursive: true, force: true }, () => { } );
